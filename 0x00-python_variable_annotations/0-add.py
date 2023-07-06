330266ece6b81ee3dd768944d2277ba349620dec
Jay stones <Joelpaul345@gmail.com>
2023-07-06 10:57:06 +0100

    :wrench: Add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!usr/bin/python3
+""" This codes shows a type-annotation"""
+
+def add(a: float, b: float) -> float:
+    """
+    Takes two float numbers as arguments and returns their sum as a float.
+
+    Args:
+        a (float): The first float number.
+        b (float): The second float number.
+
+    Returns:
+        float: The sum of the two float numbers.
+    """
+    return a + b
