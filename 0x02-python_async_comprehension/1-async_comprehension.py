aebbd4821f2c47f09308187dccfffc1c8d17cc3b
Jay stones <Joelpaul345@gmail.com>
2023-07-11 12:13:23 +0100

    git commit -m ":white_check_mark:Complete Tasks"

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 '''Task 1's module.
 '''
-import asyncio
-from random import randint
 from typing import List
+from importlib import import_module as using
 
 
-async_generator = __import__('0-async_generator').async_generator
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
