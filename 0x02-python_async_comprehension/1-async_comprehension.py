3d898b7b318d9906c703eba18085b075c4b5c3f4
Jay stones <Joelpaul345@gmail.com>
2023-07-11 12:35:03 +0100

    Update 1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-"""
-Use an asynchronous generator in an asynchronous list comprehension
-inside a coroutine
-"""
+'''Task 1's module.
+'''
 from typing import List
-import asyncio
-import random
+from importlib import import_module as using
 
-async_generator = __import__('0-async_generator').async_generator
+
+async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """Create an asynchronous list comprehension and return the list"""
-    result = [i async for i in async_generator()]
-    return result
+    '''Creates a list of 10 numbers from a 10-number generator.
+    '''
+    return [num async for num in async_generator()]
