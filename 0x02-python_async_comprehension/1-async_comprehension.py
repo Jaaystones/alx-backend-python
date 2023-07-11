2ecf11ad7ea1baa5a276efc29d281163b02869a7
Jay stones <Joelpaul345@gmail.com>
2023-07-11 12:21:46 +0100

    git commit -m ":white_check_mark:Complete Tasks"

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python3
-"""Task 1's module.
-"""
+'''Import async_generator from the previous task
+and then write a coroutine called async_comprehension
+that takes no arguments.
+'''
 from typing import List
 from importlib import import_module as using
+
 
 async_generator = using('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """
-    Coroutine that collects 10 random numbers using
+    '''Coroutine that collects 10 random numbers using
     an async comprehension over async_generator.
     Returns the 10 random numbers.
 
     Returns:
         List[int]: List of 10 random numbers.
-    """
-    return [number async for number in async_generator()]
+    '''
+    return [num async for num in async_generator()]
