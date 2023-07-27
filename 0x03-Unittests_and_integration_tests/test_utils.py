d730a3ac9fc610aa1811a3dd801318bcd4b3479e
Jay stones <Joelpaul345@gmail.com>
2023-07-28 00:24:15 +0100

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -82,7 +82,11 @@ class TestGetJson(unittest.TestCase):
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
     ])
-    def test_get_json(self, test_url, test_payload):
+    def test_get_json(
+            self,
+            test_url: str,
+            test_payload: Dict,
+            ) -> None:
         """
         Test that get_json returns the expected JSON data.
 
