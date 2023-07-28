5d1b30eadfb4e0c56f42fcaeea965c96e6070681
Jay stones <Joelpaul345@gmail.com>
2023-07-28 13:43:30 +0100

    :white_check_mark:Complete Task

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,12 +6,15 @@ from typing import Dict, Tuple, Union
 from unittest.mock import patch, Mock
 from parameterized import parameterized
 
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
+
 
 class TestAccessNestedMap(unittest.TestCase):
-    """
-    Test case for the access_nested_map function.
-    """
-
+    """Tests the `access_nested_map` function."""
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -21,64 +24,28 @@ class TestAccessNestedMap(unittest.TestCase):
             self,
             nested_map: Dict,
             path: Tuple[str],
-            expected_result: Union[Dict, int]) -> None:
-        """
-        Test the access_nested_map function with different inputs.
-
-        Parameters
-        ----------
-        nested_map : Mapping
-            A nested map (dictionary or any mapping object).
-        path : Sequence
-            A sequence of keys representing a path to the value
-            in the nested map.
-        expected_result : Any
-            The expected result when accessing the nested value.
-
-        Returns
-        -------
-        None
-        """
-        self.assertEqual(access_nested_map(nested_map, path), expected_result)
+            expected: Union[Dict, int],
+            ) -> None:
+        """Tests `access_nested_map`'s output."""
+        self.assertEqual(access_nested_map(nested_map, path), expected)
 
     @parameterized.expand([
-        ({}, ("a",), "Key 'a' not found in the nested map."),
-        ({"a": 1}, ("a", "b"), "Key 'b' not found in the nested map."),
+        ({}, ("a",), KeyError),
+        ({"a": 1}, ("a", "b"), KeyError),
     ])
     def test_access_nested_map_exception(
-              self,
-              nested_map: Dict,
-              path: Tuple[str],
-              expected_exception_msg: Exception) -> None:
-        """
-        Test that a KeyError is raised with the expected message.
-
-        Parameters
-        ----------
-        nested_map : Mapping
-            A nested map (dictionary or any mapping object).
-        path : Sequence
-            A sequence of keys representing a path to the value
-            in the nested map.
-        expected_exception_msg : str
-            The expected exception message.
-
-        Returns
-        -------
-        None
-        """
-        with self.assertRaises(KeyError) as cm:
+            self,
+            nested_map: Dict,
+            path: Tuple[str],
+            exception: Exception,
+            ) -> None:
+        """Tests `access_nested_map`'s exception raising."""
+        with self.assertRaises(exception):
             access_nested_map(nested_map, path)
-
-        self.assertEqual(str(cm.exception), expected_exception_msg)
 
 
 class TestGetJson(unittest.TestCase):
-    """
-    Test case for the get_json function.
-    """
-
-    @patch("requests.get")
+    """Tests the `get_json` function."""
     @parameterized.expand([
         ("http://example.com", {"payload": True}),
         ("http://holberton.io", {"payload": False}),
@@ -88,21 +55,7 @@ class TestGetJson(unittest.TestCase):
             test_url: str,
             test_payload: Dict,
             ) -> None:
-        """
-        Test that get_json returns the expected JSON data.
-
-        Parameters
-        ----------
-        test_url : str
-            The test URL to be used for mocking.
-        test_payload : Dict
-            The test JSON payload to be returned by the mock.
-
-        Returns
-        -------
-        None
-        """
-         """Tests `get_json`'s output."""
+        """Tests `get_json`'s output."""
         attrs = {'json.return_value': test_payload}
         with patch("requests.get", return_value=Mock(**attrs)) as req_get:
             self.assertEqual(get_json(test_url), test_payload)
